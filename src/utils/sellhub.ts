93e2dc823f0233ac8658f533e5b5beef3a8d673b
bilal-the-dev <bilalfarooq6001@gmail.com>
2025-10-06 21:22:52 +0500

    add volume

--- src/utils/sellhub.ts
+++ src/utils/sellhub.ts
@@ -17,41 +17,42 @@ export const handleSellHubOrder = async (
     addType(body);
 
     let apiResponse: orderCreateResponse = {
-      bot_invite: "",
+      bot_invite:
+        "https://dash.sellhub.cx/8e13168e-2d33-46be-aa9c-4faab7f331ae/store/notifications/",
       cost: 0,
       uniqid: "",
     }; // dummy placeholder
 
     const amountToMultiply = body.quantity;
 
-    if (
-      body.type === SELLHUB_TYPES_CONST.PREMIUM_MEMBERS ||
-      body.type === SELLHUB_TYPES_CONST.OFFLINE_MEMBERS
-    )
-      apiResponse = await createOAuthOrder({
-        service: body.type,
-        id: body.customFields[0].value,
-        amount: amountToMultiply * 20,
-      });
+    // if (
+    //   body.type === SELLHUB_TYPES_CONST.PREMIUM_MEMBERS ||
+    //   body.type === SELLHUB_TYPES_CONST.OFFLINE_MEMBERS
+    // )
+    //   apiResponse = await createOAuthOrder({
+    //     service: body.type,
+    //     id: body.customFields[0].value,
+    //     amount: amountToMultiply * 20,
+    //   });
 
-    if (body.type === SELLHUB_TYPES_CONST.ONE_MONTH_BOOST) {
-      apiResponse = await createBoostOrder(
-        BOOST_DURATION.ONE_MONTH,
-        amountToMultiply,
-        body.customFields[0].value
-      );
-    }
+    // if (body.type === SELLHUB_TYPES_CONST.ONE_MONTH_BOOST) {
+    //   apiResponse = await createBoostOrder(
+    //     BOOST_DURATION.ONE_MONTH,
+    //     amountToMultiply,
+    //     body.customFields[0].value
+    //   );
+    // }
 
-    if (body.type === SELLHUB_TYPES_CONST.THREE_MONTH_BOOST) {
-      apiResponse = await createBoostOrder(
-        BOOST_DURATION.THREE_MONTH,
-        amountToMultiply,
-        body.customFields[0].value
-      );
-    }
+    // if (body.type === SELLHUB_TYPES_CONST.THREE_MONTH_BOOST) {
+    //   apiResponse = await createBoostOrder(
+    //     BOOST_DURATION.THREE_MONTH,
+    //     amountToMultiply,
+    //     body.customFields[0].value
+    //   );
+    // }
 
     res.send(
-      `Please invite our discord bot here for your order to be processed: ${apiResponse.bot_invite}`
+      `Please invite our discord bot here for your order to be processed: [here](${apiResponse.bot_invite})`
     );
   } catch (error) {
     console.error(error);
